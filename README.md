e85c0bbb9030ef3387f528eb919d3739c5dac777
winwu <yiyingwu.1990@gmail.com>
2015-09-08 19:35:35 +0800

    fix pollutantmena

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Week2
-* specdata 資料夾底下內容請從課程網站下載
+* [specdata 資料夾底下內容請從課程網站下載](https://class.coursera.org/rprog-032/assignment/view?assignment_id=3)
+
+## 常見問題
+0.Error in read.table(file = file, header = header, sep = sep, quote = quote,  : 
+  找不到物件 'files_full' 
+  
+  Ans: 可能是還沒下載 specdata
+
--- week2/pollutantmean.R
+++ week2/pollutantmean.R
@@ -1,22 +1,17 @@
 # see also
 # https://github.com/rdpeng/practice_assignment/blob/master/practice_assignment.rmd
 
-
-
 # run this file 
 # R -f  pollutantmean.R
 # or source('~/project/R-Programming/week2/pollutantmean.R')
-
 
 # Example:
 # pollutantmean('/Users/win/project/R-Programming/week2/specdata', 'sulfate', 1:10)
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-	
 	# set dictory to your specdata folder
   setwd(directory)
-	getwd()
-  
+
   # 把 specdata 資料夾底下的檔案都列出來
   # full.names 的參數可以取得絕對路徑
 
@@ -31,14 +26,13 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 	{
 		# 每隻 csv 開始搜，rbind 是搜每一行
 		# rbind 合併 row, 一直把抓到的資料 bind 到 data 去
-		data <- rbind(data, read.csv(files_full[i]))
+		data <- rbind(data, read.csv(fules_full[i]))
 	}
 
 	# 計算平均值，至於要計算哪一欄，可能是 (sulfate or nitrate 硫酸鹽或硝酸鹽) 吧
 	# 參數 na.rm 表示是否要移除 NA 的值，預設是 false
 	# 但因題目條件，必須將 NA 刪掉
-
-	# todo: 有 colmean??
-	mean(data[, pollutantmean], na.rm = TRUE)
-
+	# 有另一方式是 colmean，但我不會 XD
+  mean(data[, pollutant], na.rm = TRUE)
+  
 }
